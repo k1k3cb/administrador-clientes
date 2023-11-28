04d8c84f00048cf5f639e4ba8f702717ec4b86a6
k1k3cb <enriquecbar@gmail.com>
2023-11-28 14:16:24 +0100

    layout

--- src/App.jsx
+++
@@ -1,5 +0,0 @@
-const App = () => {
-	return <h1>Núcleo de la aplicación</h1>;
-};
-
-export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,27 @@
 import { createRoot } from 'react-dom/client';
-import App from './App';
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
+import Layout from './components/Layout';
+import NuevoCliente from './pages/NuevoCliente';
+import Index from './pages/Index';
 
 const container = document.getElementById('root');
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <Layout />,
+		children: [
+			{
+				index: true,
+				element: <Index />
+			},
+			{
+				path: '/clientes/nuevo',
+				element: <NuevoCliente />
+			}
+		]
+	}
+]);
 
-createRoot(container).render(<App />);
+createRoot(container).render(<RouterProvider router={router} />);
